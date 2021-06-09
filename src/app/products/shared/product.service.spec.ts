1310b92de8609a3554ce32fa8b084e061701f3d3
Oleksandr Mandryk <mandrytech@gmail.com>
2021-06-09 16:21:42 +0200

    did stuff

--- src/app/products/shared/product.service.spec.ts
+++ src/app/products/shared/product.service.spec.ts
@@ -3,7 +3,8 @@ import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestor
 import { FileService } from 'src/app/files/shared/file.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ProductService } from './product.service';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { Product } from './product.model';
 
 describe('ProductService', () => {
     let angularFirestoreMock: any;
@@ -11,8 +12,10 @@ describe('ProductService', () => {
     let httpMock: HttpTestingController;
     let service: ProductService;
     let fsCollectionMock: any;
+    let helper: Helper;
 
   beforeEach(() => {
+    helper = new Helper();
     angularFirestoreMock = jasmine.createSpyObj('AngularFirestore', ['collection']);
     fsCollectionMock = jasmine.createSpyObj('collection', ['snapshotChanges', 'valueChanges']);
     angularFirestoreMock.collection.and.returnValue(fsCollectionMock);
@@ -42,7 +45,8 @@ describe('ProductService', () => {
 
     beforeEach(() => {
         service.getProducts();
-    })
+    }); 
+
     it('should call collection 1 time on AngularFirestore service', () => {
         expect(angularFirestoreMock.collection).toHaveBeenCalledTimes(1);
     });
@@ -54,6 +58,70 @@ describe('ProductService', () => {
       it('should call snapshotChanges 1 time on AngularFirestore service', () => {
         expect(fsCollectionMock.snapshotChanges).toHaveBeenCalledTimes(1);
       });
-  })
+  });
+
+  describe("getProducts Return Value", () => {
+    it('should call getProducts return single product', () => {
+        fsCollectionMock.snapshotChanges.and.returnValue(helper.getActions(1));
+        service.getProducts().subscribe(products => {
+            expect(products.length).toBe(1)
+        })
+    });
+    
+
+    it('should call getProducts return 10 products', () => {
+        fsCollectionMock.snapshotChanges.and.returnValue(helper.getActions(10));
+        service.getProducts().subscribe(products => {
+            expect(products.length).toBe(10)
+        })
+    });
+  });
+
+  it('should call getProducts return single product with correct properties', () => {
+    fsCollectionMock.snapshotChanges.and.returnValue(helper.getActions(1));
+    service.getProducts().subscribe(products => {
+        expect(products[0]).toEqual({
+            id: 'abc0',
+            name: 'abc0',
+            pictureId: 'def0',
+        })
+    })
+});
 
 });
+
+
+
+class Helper {
+    actions = [];
+
+    getActions(amount: number, addPictureId: boolean = true): Observable<any> {
+        for(let i = 0; i < amount; i++) {
+            /**
+             *  const data = action.payload.doc.data() as Product;
+                    return {
+                        id: action.payload.doc.id,
+                        name: data.name,
+                        pictureId: data.pictureId
+                    };
+             */
+            const product: Product = {
+                id: 'abc',
+                name: 'abc' + i
+            }
+            if(addPictureId){
+                product.pictureId = 'def'+i;
+            }
+            this.actions.push({
+                payload: {
+                    doc: {
+                        id: 'abc' + i,
+                        data: () => product
+                        }
+                    }
+                })
+            }
+            return of(this.actions);
+        }
+
+}
